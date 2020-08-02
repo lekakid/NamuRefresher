76fe9110ab4c5cfad593cfd24e193c9826d82249
LeKAKiD <lekakid@kakao.com>
2020-08-02 17:34:00 +0900

    버전 갱신

--- NamuRefresher.user.js
+++ NamuRefresher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        NamuRefresher
 // @author      LeKAKiD
-// @version     1.9.0
+// @version     1.9.1
 // @include     https://arca.live/*
 // @include     https://*.arca.live/*
 // @run-at      document-start
